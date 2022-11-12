2a0caa3304f1d2d9de350b1e713b26cb54715095
SebastianKnapovsky <91384942+SebastianKnapovsky@users.noreply.github.com>
2022-11-12 18:51:40 +0100

    Dodanie przypadkowo usuniętego zadania lab1 + zadanie lab3

---
+++ lab3/script.js
@@ -0,0 +1,33 @@
+document.addEventListener('keypress', onKeyPress);
+
+const KeyToSound = {
+    'a': document.querySelector('#clap'),
+    'A': document.querySelector('#clap'),
+    's': document.querySelector('#boom'),
+    'S': document.querySelector('#boom'),
+    'd': document.querySelector('#hihat'),
+    'D': document.querySelector('#hihat'),
+    'f': document.querySelector('#kick'),
+    'F': document.querySelector('#kick'),
+    'g': document.querySelector('#openhat'),
+    'G': document.querySelector('#openhat'),
+    'h': document.querySelector('#ride'),
+    'H': document.querySelector('#ride'),
+    'j': document.querySelector('#snare'),
+    'J': document.querySelector('#snare'),
+    'k': document.querySelector('#tink'),
+    'K': document.querySelector('#tink'),
+    'l': document.querySelector('#tom'),
+    'L': document.querySelector('#tom')
+}
+
+
+function onKeyPress(event){
+    const sound = KeyToSound[event.key];
+    playSound(sound);
+}
+
+function playSound(sound){
+    sound.currentTime = 0;
+    sound.play();
+}
